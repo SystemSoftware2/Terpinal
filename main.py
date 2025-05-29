a2e1e34cb296b91aeabdf0bff02ad995a204d29a
System <dimasoft976@gmail.com>
2025-05-29 14:57:38 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+from machine import *
+from ssd1306 import SSD1306_I2C
+from utime import *
+from keyboard import *
+from playsong import *
+from handler import *
+import sys
+
+i2c=I2C(0,sda=Pin(0), scl=Pin(1), freq=400000)
+oled = SSD1306_I2C(128, 64, i2c)
+
+oled.text("help - B,", 0, 0)
+oled.text("enter - *", 0, 10)
+oled.show()
+i += 20
+
+prog = ''
+while True:
+    key = keypadRead()
+    if key == 'enter':
+        code = prog
+        prog = ''
+        run(oled, melody, playsong, code)
+    elif key != -1:
+        prog += str(key)
+        oled.text(key, j, i)
+        oled.show()
+        j += len(str(key)) * 10
