a584d7e62c23e13b3c5eb6cd41276e150752bdc0
System <dimasoft976@gmail.com>
2025-05-29 14:58:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+'''
+Основной файл терпинала.
+Запускать его.
+'''
+
 from machine import *
 from ssd1306 import SSD1306_I2C
 from utime import *
@@ -6,14 +11,17 @@ from playsong import *
 from handler import *
 import sys
 
-i2c=I2C(0,sda=Pin(0), scl=Pin(1), freq=400000)
+#переменные
+i2c = I2C(0 ,sda=Pin(0), scl=Pin(1), freq=400000)
 oled = SSD1306_I2C(128, 64, i2c)
 
+#вступление
 oled.text("help - B,", 0, 0)
 oled.text("enter - *", 0, 10)
 oled.show()
 i += 20
 
+#цикл
 prog = ''
 while True:
     key = keypadRead()
